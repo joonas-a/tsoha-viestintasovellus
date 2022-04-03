fbd8fe413eb40a50ce6e1222b0740b60eef2a166
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2022-04-03 21:15:29 +0300

    Add account creation

--- routes.py
+++ routes.py
@@ -1,9 +1,41 @@
 from app import app
 from flask import render_template, request, redirect, session
-import boards
+import boards, users
 
 
 @app.route("/")
 def index():
     list = boards.get_boards()
     return render_template("index.html", count=len(list), boards=list)
+
+@app.route("/logout")
+def logout():
+    users.logout()
+    return redirect("/")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Username or password is wrong")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="The passwords did not match")
+        if users.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registration failed")
